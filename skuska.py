a6e1d97a16613194e3d7df4fcbf8f6f56dc47a44
AdamMiko123 <xmikoa@stuba.sk>
2022-06-15 01:40:45 +0200

    upratanie kodu, tlacidla start, stop a close, doplnenie vypisu

--- skuska.py
+++ skuska.py
@@ -29,8 +29,9 @@ def background_thread(args):
         socketio.sleep(1)
         count += 1
         read_ser_data = read_ser.split(" ")
+        read_ser_data[3] = read_ser_data[3].replace("\r\n","")
         print(read_ser_data)
-        if btnV == "start":
+        if btnV == "Start":
             socketio.emit('my_response',{'distance': float(read_ser_data[0]), 'humidity': float(read_ser_data[1]),
                                          'temperature': float(read_ser_data[2]), 'photo': float(read_ser_data[3]), 'count': count},
                           namespace='/test')  
@@ -38,32 +39,28 @@ def background_thread(args):
 @app.route('/', methods=['GET', 'POST'])
 def skuska():
     return render_template('skuska.html', async_mode=socketio.async_mode)
-  
+
+@socketio.on('click_event', namespace='/test')
+def click_event(message):   
+    session['btn_value'] = message['value'] 
+
 @socketio.on('my_event', namespace='/test')
-def test_message(message):   
+def my_event(message):   
     session['Vyber'] = message['value']  
     #emit('my_response', {'Distance': session['Distance'], 'Humidity': session['Humidity'], 'Temperature': session['Temperature'], 'Photo': session['Photo']})
  
-@socketio.on('disconnect_request', namespace='/test')
-def disconnect_request():
-    emit('my_response', {'data': 'Disconnected!'})
+@socketio.on('close', namespace='/test')
+def close():
+    emit('my_response', {'data': 'Closed'})
     disconnect()
 
 @socketio.on('connect', namespace='/test')
-def test_connect():
+def connect():
     global thread
     with thread_lock:
         if thread is None:
             thread = socketio.start_background_task(target=background_thread, args=session._get_current_object())
-    emit('my_response', {'data': 'Connected', 'count': 0})
-
-@socketio.on('click_event', namespace='/test')
-def db_message(message):   
-    session['btn_value'] = message['value']    
-
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    print('Client disconnected', request.sid)
+    emit('my_response', {'data': 'Connected', 'count': 0})   
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=80, debug=True)
